742f92bfe7af8cc7039e7f1d2795eacaf477e98c
sbx_user1051 <sbx_user1051@169.254.183.125>
2022-03-19 17:58:58 +0000

    Sat Mar 19 17:58:58 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,18 +8,28 @@ PRICES = {
 }
 
 OFFERS = {
-    'A': [(3, 130)],
-    'B': [(2, 45)]
+    'A': {3: 130},
+    'B': {2: 45}
 }
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    total = 0
     sku_counter = Counter(skus)
-    print(sku_counter)
+    for sku in sku_counter:
+        count_sku = sku_counter[sku]
+        if sku in OFFERS:
+            offers = sorted(OFFERS[sku].keys(), reverse=True)
+            for offer in offers:
+                while offer >= count_sku:
+                    total += OFFERS[sku][offer]
+                    count_sku -= offer
 
+    return total
 
 if __name__ == '__main__':
     order1 = 'ABC'
     assert checkout(order1) == 100
+
 
