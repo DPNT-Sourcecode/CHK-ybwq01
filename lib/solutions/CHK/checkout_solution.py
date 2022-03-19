61a4fcf033f0d860ce46b109c4e8e488a172caaa
sbx_user1051 <sbx_user1051@169.254.74.189>
2022-03-19 18:57:33 +0000

    Sat Mar 19 18:57:33 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -22,7 +22,12 @@ CROSS_OFFERS = {
 def checkout(skus):
     total = 0
     sku_counter = Counter(skus)
-    for sku in sku_counter.keys():
+    skus_order = []
+    for sku in sku_counter:
+        if sku in CROSS_OFFERS:
+            skus_order.append(sku)
+    skus_order.extend([sku for sku in list(sku_counter.keys()) if sku not in skus_order])
+    for sku in skus_order:
         if sku not in PRICES:
             return -1
         count_sku = sku_counter[sku]
@@ -33,7 +38,6 @@ def checkout(skus):
                     total += OFFERS[sku][offer]
                     count_sku -= offer
         total += count_sku * PRICES[sku]
-        # print(total)
         if sku in CROSS_OFFERS:
             remaining = sku_counter[sku]
             offers = sorted(CROSS_OFFERS[sku].keys(), reverse=True)
@@ -59,8 +63,7 @@ def checkout(skus):
 #     assert checkout(order5) == 80
 #     order6 = 'EEEEBB'
 #     assert checkout(order6) == 160
-
-
-
+#     order7 = 'BEBEEE'
+#     assert checkout(order7) == 160
 
 
