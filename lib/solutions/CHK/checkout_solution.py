1b12fdde603c89763c04132790dd8b4efedb60ea
sbx_user1051 <sbx_user1051@169.254.183.125>
2022-03-19 18:01:58 +0000

    Sat Mar 19 18:01:58 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -18,18 +18,23 @@ def checkout(skus):
     total = 0
     sku_counter = Counter(skus)
     for sku in sku_counter:
+        if sku not in PRICES:
+            return -1
         count_sku = sku_counter[sku]
         if sku in OFFERS:
             offers = sorted(OFFERS[sku].keys(), reverse=True)
             for offer in offers:
-                while offer >= count_sku:
+                while count_sku >= offer:
                     total += OFFERS[sku][offer]
                     count_sku -= offer
+        total += count_sku * PRICES[sku]
 
     return total
 
 if __name__ == '__main__':
-    order1 = 'ABC'
+    order1 = 'AAAAAAA'
+    print(checkout(order1))
     assert checkout(order1) == 100
+
 
 
